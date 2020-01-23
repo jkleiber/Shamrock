3beb1d97c0389e40e50a0ad7e2b535ef1124dc0e
Justin Kleiber <jkleiber8@gmail.com>
2020-01-23 12:52:49 -0600

    Begins work on implementing the EKF

--- firmware/DriveModule/DriveModule.ino
+++ firmware/DriveModule/DriveModule.ino
@@ -1,19 +1,18 @@
 /**
  * @file DriveModule.ino
  * @author Justin Kleiber (jkleiber8@gmail.com)
- * @brief Controlling Shamrock's drive train and providing 
+ * @brief Controlling Shamrock's drive train and providing
  *        data to the main board
  * @version 0.1
  * @date 2019-08-03
- * 
+ *
  * @copyright Copyright (c) 2019
- * 
+ *
  */
 
-// ROS Includes
-#include <ros.h>
 
-// RobotLib
+// Arduino dependencies
+#include <ArduinoJson.h>
 #include <RobotLib.h>
 
 // Shamrock Drive Module Code
@@ -23,8 +22,6 @@
 #include "SensorManagement.h"
 
 
-// Create the drive node
-ros::NodeHandle_<ArduinoHardware, 1, 1, 80, 80> drive_node;
 
 
 // Track control loop timing
@@ -33,14 +30,10 @@ unsigned long last_loop_time;
 
 /**
  * @brief Set up the program
- * 
+ *
  */
 void setup()
 {
-    // Initialize the ROS node and advertise publishers
-    drive_node.initNode();
-    drive_node.advertise(sensor_data_pub);
-
     // Initialize the devices
     device_init();
 
@@ -51,15 +44,16 @@ void setup()
 
 /**
  * @brief Main program loop
- * 
+ *
  */
 void loop()
 {
     // Update sensor data
     sensor_update();
 
-    // Pump callbacks
-    drive_node.spinOnce();
+    // TODO: send sensor data to raspberry pi
+
+    // TODO: receive commands from raspberry pi
 
     // Control drivetrain
     drive_control();
--- firmware/DriveModule/SensorManagement.h
+++ firmware/DriveModule/SensorManagement.h
@@ -1,17 +1,10 @@
 #ifndef SENSOR_MANAGEMENT_H
 #define SENSOR_MANAGEMENT_H
 
-// ROS Includes
-#include <ros.h>
-#include <shamrock_msgs/SensorData.h>
 
 // Shamrock sensors
 #include "Devices.h"
 #include "DriveModuleConstants.h"
-
-// Send sensor data back to the raspberry pi
-shamrock_msgs::SensorData sensor_data;
-ros::Publisher sensor_data_pub("shamrock_drive/sensors", &sensor_data);
 
 
 void sensor_update()
